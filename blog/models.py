1cd5d8bffaf468b3af5a9a49eb547bc82216ed66
Daniel Tsega <116147211+DannySanchez6658@users.noreply.github.com>
2024-06-23 16:09:41 +0300

    fix(models.py): fix spelling error

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
-    published = models.DateTimeField(default=timezone.now)
+    publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
