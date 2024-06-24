10de945aeffcc042779027ebb98a6874006e6ff6
Daniel Tsega <116147211+DannySanchez6658@users.noreply.github.com>
2024-06-24 16:36:13 +0300

    feat(models.py): add published model manager that overrides the default objects manager

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,12 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+                        .filter(status=Post.Status.PUBLISHED)
+
 class Post(models.Model):
 
     # Status field class
@@ -19,6 +25,10 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
