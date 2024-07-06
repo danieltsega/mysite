c630e314ad4a66a45909da011c45abac731689ba
Daniel Tsega <116147211+DannySanchez6658@users.noreply.github.com>
2024-07-06 18:41:15 +0300

    feat(views.py): add exception handling for page out of range and invalid page number

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
@@ -10,7 +10,12 @@ def post_list(request):
     # Add pagination to post list
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
 
     return render(request, 'blog/post/list.html', {'posts': posts})
 
